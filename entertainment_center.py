db07f89e2dd08cbae68d9ca4d093fc38fd7ef092
s97712 <s97712>
2018-01-01 13:52:25 +0800

    modified:   .gitignore 	modified:   entertainment_center.py 	modified:   media.py

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,16 +1,42 @@
+
 import fresh_tomatoes
 import media
 import test_web_server
 
 
-movies = media.generate_movies(); #生成电影列表
+""" 调试时使用 """
+debug = 0;
 
 
-debug = 1
+if __name__ == "__main__":
 
-if debug:
-	content = fresh_tomatoes.create_page_content(movies);
-	test_web_server.start("192.168.56.101",4780,content);
-else:
-	fresh_tomatoes.open_movies_page(movies);
-	
+	movies = [
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924"),
+		media.Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924")
+	];
+
+
+	if debug:
+		content = fresh_tomatoes.create_page_content(movies);
+		test_web_server.start("192.168.56.101",4780,content);
+	else:
+		fresh_tomatoes.open_movies_page(movies);
+		
--- media.py
+++ media.py
@@ -1,30 +1,16 @@
 
-# Movie结构
-# {
-#	trailer_url
-#	title
-#	poster_image_url
-# }
-
-
+""" 构造生成html所需要的movie结构 """
 class Movie:
+	"""
+	Args:
+		title:	电影的标题
+		id:		优酷视频的show_id
+		image:	优酷的图片id
+
+	"""
+
 	def __init__(self, title, id, image):
-		self.title = title;
+		self.title = title;	
 		self.trailer_url = "http://v.youku.com/v_show/id_"+ id+ "==.html?";
-		#self.trailer_url = "http://v.youku.com/v_show/id_" + id;
 		self.poster_image_url = "http://r1.ykimg.com/" + image;
-
-
-def generate_movies():
-	movies = [];
-	movie = Movie("艳骨", "XMzA5NzkxNTYzMg", "051000005A46D826ADC0AE078B010924");
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	movies.append(movie);
-	return movies;
+	
